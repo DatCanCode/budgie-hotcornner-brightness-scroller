f9e45a3403224c2d0dacc4e7e07966f30f805923
Datami555 <Datami.555@gmail.com>
2019-04-15 12:24:23 +0700

    change the way to control volume

--- src/ScrollControlBrightnessVolume.py
+++ src/ScrollControlBrightnessVolume.py
@@ -10,10 +10,13 @@ mouseEvent = '/dev/input/event8'
 # amount of mouse movement to exit program
 thresholdDistant = 30
 
-# default keytype changes brightness
-keytype = "brightness"
+# a percentage change in volume
+volumeStep = "5"
+
+# default controlType changes brightness
+controlType = "brightness"
 if len(sys.argv) > 1 and sys.argv[1] == "volume":
-    keytype = "volume"
+    controlType = "volume"
 
 # read event of mouse from syste,
 dev = InputDevice(mouseEvent)
@@ -21,20 +24,24 @@ dev = InputDevice(mouseEvent)
 # total mouse movement distance
 amountOfMovement = 0
 
-# multimedia keys for change volume and brightness
-# each type has a set. 1st element to turn down, the other to turn up
-keys = {
-    "brightness": ["XF86MonBrightnessDown", "XF86MonBrightnessUp"],
-    "volume": ["XF86AudioLowerVolume", "XF86AudioRaiseVolume"]
-}
+# multimedia keys to change brightness
+# 1st element to turn down, the other to turn up
+keys = ["XF86MonBrightnessDown", "XF86MonBrightnessUp"]
 
-def ChangeBrightnessVolume(scrollValue, keys, keytype):
-    # update brightness and volume
-    
-    key = keys[keytype][scrollValue > 0]
+def ChangeBrightness(scrollValue, keys):
+    # get 1st element which decreases the brightness when scrollValue < 0 or vice versa
+    key = keys[scrollValue > 0]
 
     # send key using xdotool
     subprocess.run(["xdotool", "key", key])
+
+def ChangeVolume(scrollValue, volumeStep):
+    # percentage will be positive if scrollValue positive or vice versa
+    vary = ['-', '+']
+    percentage = ''.join([volumeStep, "%", vary[scrollValue > 0]])
+
+    # send command to adjust volume
+    subprocess.run(["amixer", "-qD", "pulse", "sset", "Master", percentage])
 
 
 # parse mouse events
@@ -47,4 +54,7 @@ for event in dev.read_loop():
             if amountOfMovement > thresholdDistant: exit(0)
         # track for mouse scroll to change brightness or volume
         elif event.code == ecodes.REL_WHEEL:
-            ChangeBrightnessVolume(event.value, keys, keytype)
+            if controlType == "brightness":
+                ChangeBrightness(event.value, keys)
+            else:
+                ChangeVolume(event.value, volumeStep)
